89dd8c19db6e535e713afb151b7aba0998ce0e23
Fuflick <stepan_belov_2016@mail.ru>
2024-04-23 10:56:41 +0300

    dbContext

--- TeleBase/MyDbContext.cs
+++ TeleBase/MyDbContext.cs
@@ -1,9 +1,24 @@
 using Microsoft.EntityFrameworkCore;
+using TeleBase.Models;
 
 namespace TeleBase;
 
 public class MyDbContext : DbContext
 {
+    public DbSet<Bot> Bots { get; set; } = null!;
+
+    public DbSet<Chat> Chats { get; set; } = null!;
+
+    public DbSet<Command> Commands { get; set; } = null!;
+
+    public DbSet<User> Users { get; set; } = null!;
+
+    public DbSet<BotChat> BotChats { get; set; } = null!;
+
+    public DbSet<BotCommand> BotCommands { get; set; } = null!;
+
+    public DbSet<UserChat> UserChats { get; set; } = null!;
+
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
         builder.UseNpgsql("Host=172.17.0.3;Database=TelegramBase;Username=postgres;Password=123");

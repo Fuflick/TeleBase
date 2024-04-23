1e1491c3f46f9346b7a6750e2acaa983689d63e2
Fuflick <stepan_belov_2016@mail.ru>
2024-04-23 10:41:02 +0300

    botChat

--- TeleBase/Models/BotChat.cs
+++ TeleBase/Models/BotChat.cs
@@ -1,6 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TeleBase.Models;
 
 public class BotChat
 {
-    
+    [Key] public int Id { get; set; }
+
+    public int? BotId { get; set; }
+
+    public int? ChatId { get; set; }
 }
